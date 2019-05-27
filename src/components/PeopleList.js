86f1807fefcb40539edd852f0f08ceed2b1307b4
Tiago Zuege <tiago.zuege@gmail.com>
2019-05-26 22:54:02 -0300

    [56] Criação de estilos para componente PeopleList

--- src/components/PeopleList.js
+++ src/components/PeopleList.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import {View, Text} from 'react-native';
+import {View, Text, StyleSheet} from 'react-native';
 
 
 /* Sem arrow function */
@@ -8,14 +8,18 @@ const PeopleList = function(props) {
     const people = props.people;
     const textElements = people.map(function (element) {
         const firstName = element.name.first;
-        return <Text key={firstName}>{firstName}</Text>;
+        return (
+            <View key={ firstName } style={ styles.line }>
+                <Text style={styles.lineText}>{ firstName }</Text>
+            </View>
+        );
     });
 
     // Usar 'key' em elementos de lista para que o react possa
     // diferenciar os elementos. Ver https://fb.me/react-warning-keys
 
     return (
-        <View>
+        <View style={styles.container}>
             { textElements }
         </View>
     )
@@ -25,5 +29,22 @@ const PeopleList = function(props) {
 // const PeopleList = props => (
 //     <Text></Text>
 // )
+
+const styles = StyleSheet.create({
+    container: {
+        backgroundColor: '#d0dcef'
+    },
+    line: {
+        height: 40,
+        borderBottomWidth: 1,
+        borderBottomColor: '#bbb',
+        alignItems: 'center',
+        flexDirection: 'row'
+    },
+    lineText: {
+        fontSize: 20,
+        paddingLeft: 15
+    }
+});
 
 export default PeopleList;
