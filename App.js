cc9154c6a7f06fab4bb09cc659741157471a7cbc
Tiago Zuege <tiago.zuege@gmail.com>
2019-06-30 22:57:45 -0300

    [67] Pacote react-navigation: adicionado funcionalidade de navegação de páginas

--- App.js
+++ App.js
@@ -1,3 +1,12 @@
+import {createAppContainer, createStackNavigator} from "react-navigation";
 import PeoplePage from './src/pages/PeoplePage';
 
-export default PeoplePage;
+const AppNavigator = createStackNavigator({
+    'Main': {
+        'screen': PeoplePage
+    }
+});
+
+const AppContainer = createAppContainer(AppNavigator);
+
+export default AppContainer;
